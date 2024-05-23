28bb86c4bea3e966751ab58553a491b839ed5eb4
devChary <jagannadhachary7182@gmail.com>
2024-05-23 14:26:41 +0530

    feat: add constants for employee and gateway options

---
+++ src/modules/constants.js
@@ -0,0 +1,35 @@
+const GATEWAY_OPTIONS = [
+  {
+    label: "Enter",
+    value: "ENTER",
+  },
+  {
+    label: "Exit",
+    value: "EXIT",
+  },
+];
+
+const EMPLOYEE_OPTIONS = [
+  {
+    label: "Peter",
+    value: "Peter",
+  },
+  {
+    label: "Glenn",
+    value: "Glenn",
+  },
+  {
+    label: "Stewie",
+    value: "Stewie",
+  },
+  {
+    label: "Brain",
+    value: "Brain",
+  },
+  {
+    label: "Lois",
+    value: "Lois",
+  },
+];
+
+export { GATEWAY_OPTIONS, EMPLOYEE_OPTIONS };
